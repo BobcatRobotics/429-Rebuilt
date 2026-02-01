96d840abe1d26da2fffd2147b9f554857ec4f493
aopalka <79727669+aopalka@users.noreply.github.com>
2026-02-01 13:25:05 -0500

    Migrated as a template
    Migrates the Everybot code too the correct 2026 location and created a template for students to work on that contains the right "structure" of the code.

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Template";
+  public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-01-12 20:25:21 EST";
-  public static final long BUILD_UNIX_TIME = 1768267521069L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "bff956326dcd8bceaa4ff25a393ebf9b42f2f697";
+  public static final String GIT_DATE = "2026-02-01 12:05:34 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-01 12:45:29 EST";
+  public static final long BUILD_UNIX_TIME = 1769967929890L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
--- comp/2026/src/main/java/frc/robot/RobotContainer.java
+++ comp/2026/src/main/java/frc/robot/RobotContainer.java
@@ -23,17 +23,28 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 // import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Constants.ClimbConstatns;
+import frc.robot.Constants.FuelConstants;
 import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.climber.Climber;
+import frc.robot.subsystems.climber.ClimberIO;
+import frc.robot.subsystems.climber.ClimberIOReal;
+import frc.robot.subsystems.climber.ClimberIOSim;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.GyroIO;
 import frc.robot.subsystems.drive.GyroIOPigeon2;
 import frc.robot.subsystems.drive.ModuleIO;
 import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.drive.ModuleIOTalonFX;
+import frc.robot.subsystems.fuel.Fuel;
+import frc.robot.subsystems.fuel.FuelIO;
+import frc.robot.subsystems.fuel.FuelIOReal;
+import frc.robot.subsystems.fuel.FuelIOSim;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.VisionIOLimelight;
 import org.bobcatrobotics.Commands.ActionFactory;
@@ -42,142 +53,190 @@ import org.bobcatrobotics.GameSpecific.Rebuilt.HubUtil;
 import org.bobcatrobotics.Subsystems.AntiTippingLib.AntiTipping;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // Subsystems
-  private final Drive drive;
-  private final AntiTipping antiTipping;
-  private Vision vision;
+    // Subsystems
+    private final Fuel fuel;
+    private final Climber climber;
+    private final Drive drive;
+    private final AntiTipping antiTipping;
+    private Vision vision;
 
-  // Controller
-  private final CommandXboxController controller = new CommandXboxController(0);
+    // Controller
+    private final CommandXboxController controller = new CommandXboxController(0);
+    private final CommandXboxController operator = new CommandXboxController(1);
 
-  // Dashboard inputs
-  private final LoggedDashboardChooser<Command> autoChooser;
+    // Dashboard inputs
+    private final LoggedDashboardChooser<Command> autoChooser;
 
-  private final HubUtil hub;
+    private final HubUtil hub;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    switch (Constants.currentMode) {
-      case REAL:
-        // Real robot, instantiate hardware IO implementations
-        drive = new Drive(new GyroIOPigeon2(), new ModuleIOTalonFX(TunerConstants.FrontLeft),
-            new ModuleIOTalonFX(TunerConstants.FrontRight),
-            new ModuleIOTalonFX(TunerConstants.BackLeft),
-            new ModuleIOTalonFX(TunerConstants.BackRight));
-        // Vision
-        vision =
-            new Vision(drive::addVisionMeasurement, new VisionIOLimelight("", drive::getRotation));
-        break;
-      case SIM:
-        // Sim robot, instantiate physics sim IO implementations
-        drive = new Drive(new GyroIO() {}, new ModuleIOSim(TunerConstants.FrontLeft),
-            new ModuleIOSim(TunerConstants.FrontRight), new ModuleIOSim(TunerConstants.BackLeft),
-            new ModuleIOSim(TunerConstants.BackRight));
-        break;
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        switch (Constants.currentMode) {
+            case REAL:
+                // Real robot, instantiate hardware IO implementations
+                drive = new Drive(new GyroIOPigeon2(), new ModuleIOTalonFX(TunerConstants.FrontLeft),
+                        new ModuleIOTalonFX(TunerConstants.FrontRight),
+                        new ModuleIOTalonFX(TunerConstants.BackLeft),
+                        new ModuleIOTalonFX(TunerConstants.BackRight));
+                fuel = new Fuel(new FuelIOReal());
+                climber = new Climber(new ClimberIOReal());
+                // Vision
+                vision = new Vision(drive::addVisionMeasurement, new VisionIOLimelight("", drive::getRotation));
+                break;
+            case SIM:
+                // Sim robot, instantiate physics sim IO implementations
+                drive = new Drive(new GyroIO() {
+                }, new ModuleIOSim(TunerConstants.FrontLeft),
+                        new ModuleIOSim(TunerConstants.FrontRight), new ModuleIOSim(TunerConstants.BackLeft),
+                        new ModuleIOSim(TunerConstants.BackRight));
+                fuel = new Fuel(new FuelIOSim());
+                climber = new Climber(new ClimberIOSim());
+                break;
 
-      default:
-        // Replayed robot, disable IO implementations
-        drive = new Drive(new GyroIO() {}, new ModuleIO() {}, new ModuleIO() {}, new ModuleIO() {},
-            new ModuleIO() {});
-        break;
+            default:
+                // Replayed robot, disable IO implementations
+                drive = new Drive(new GyroIO() {
+                }, new ModuleIO() {
+                }, new ModuleIO() {
+                }, new ModuleIO() {
+                },
+                        new ModuleIO() {
+                        });
+                fuel = new Fuel(new FuelIO() {
+                });
+                climber = new Climber(new ClimberIO() {
+                });
+                break;
+        }
+
+        antiTipping = new AntiTipping(() -> drive.getPitch(), () -> drive.getRoll(), 0.04, // kP
+                3.0, // tipping threshold (degrees)
+                2.5 // max correction speed (m/s)
+        );
+
+        // Set up auto routines
+        autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+
+        // Set up SysId routines
+        autoChooser.addOption("Drive Wheel Radius Characterization",
+                DriveCommands.wheelRadiusCharacterization(drive));
+        autoChooser.addOption("Drive Simple FF Characterization",
+                DriveCommands.feedforwardCharacterization(drive));
+        autoChooser.addOption("Drive SysId (Quasistatic Forward)",
+                drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+        autoChooser.addOption("Drive SysId (Quasistatic Reverse)",
+                drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+        autoChooser.addOption("Drive SysId (Dynamic Forward)",
+                drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+        autoChooser.addOption("Drive SysId (Dynamic Reverse)",
+                drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+
+        // Configure the button bindings
+        configureButtonBindings();
+
+        hub = new HubUtil();
     }
 
-    antiTipping = new AntiTipping(() -> drive.getPitch(), () -> drive.getRoll(), 0.04, // kP
-        3.0, // tipping threshold (degrees)
-        2.5 // max correction speed (m/s)
-    );
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
+     * instantiating a {@link GenericHID} or one of its subclasses
+     * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+     * passing it to a
+     * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
 
-    // Set up auto routines
-    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+        // Default command, normal field-relative drive
+        drive.setDefaultCommand(
+                DriveCommands.joystickDrive(
+                        drive,
+                        () -> -controller.getLeftY(),
+                        () -> -controller.getLeftX(),
+                        () -> -controller.getRightX()));
 
-    // Set up SysId routines
-    autoChooser.addOption("Drive Wheel Radius Characterization",
-        DriveCommands.wheelRadiusCharacterization(drive));
-    autoChooser.addOption("Drive Simple FF Characterization",
-        DriveCommands.feedforwardCharacterization(drive));
-    autoChooser.addOption("Drive SysId (Quasistatic Forward)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption("Drive SysId (Quasistatic Reverse)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption("Drive SysId (Dynamic Forward)",
-        drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption("Drive SysId (Dynamic Reverse)",
-        drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+        fuel.setDefaultCommand(fuel.run(() -> fuel.stop()));
+        climber.setDefaultCommand(climber.run(() -> climber.stop()));
 
-    // Configure the button bindings
-    configureButtonBindings();
+        // Lock to 0° when A button is held
+        controller
+                .a()
+                .whileTrue(
+                        DriveCommands.joystickDriveAtAngle(
+                                drive,
+                                () -> -controller.getLeftY(),
+                                () -> -controller.getLeftX(),
+                                () -> Rotation2d.kZero));
 
-    hub = new HubUtil();
-  }
+        // Switch to X pattern when X button is pressed
+        controller.x()
+                .onTrue(new ActionFactory().singleAction("X-Command", () -> drive.stopWithX(), drive));
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses
-   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
+        // Reset gyro to 0° when B button is pressed
+        controller.b()
+                .onTrue(new ActionFactory().singleAction("ZeroGyroCommand",
+                        () -> drive.setPose(new Pose2d(drive.getPose().getTranslation(), Rotation2d.kZero)),
+                        drive).ignoringDisable(true));
 
-    // Default command, normal field-relative drive
-    drive.setDefaultCommand(
-        DriveCommands.joystickDrive(
-            drive,
-            () -> -controller.getLeftY(),
-            () -> -controller.getLeftX(),
-            () -> -controller.getRightX()));
+        // Antitipping
+        controller.leftBumper()
+                .whileTrue(new ActionFactory().continuousAction("DriveWithAntiTipping",
+                        () -> DriveCommands.joystickDriveWithAntiTipping(drive, () -> -controller.getLeftY(),
+                                () -> -controller.getLeftX(), () -> -controller.getRightX(), antiTipping),
+                        () -> DriveCommands.joystickDriveWithAntiTipping(drive, () -> 0, () -> 0, () -> 0,
+                                antiTipping)));
 
-    // Lock to 0° when A button is held
-    controller
-        .a()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
-                drive,
-                () -> -controller.getLeftY(),
-                () -> -controller.getLeftX(),
-                () -> Rotation2d.kZero));
+        operator.leftBumper().whileTrue(Commands.run(() -> {
+            fuel.setIntakeLauncherRoller(FuelConstants.INTAKE_INTAKING_PERCENT);
+            fuel.setFeederRoller(FuelConstants.INDEXER_INTAKING_PERCENT);
+        }, fuel)).onFalse(Commands.runOnce(() -> fuel.stop(), fuel));
+        operator.rightBumper().whileTrue(Commands.run(() -> {
+            fuel.setIntakeLauncherRoller(FuelConstants.LAUNCHING_LAUNCHER_PERCENT);
+            fuel.setFeederRoller(FuelConstants.INDEXER_SPIN_UP_PRE_LAUNCH_PERCENT);
+        }, fuel).withTimeout(FuelConstants.SPIN_UP_SECONDS).andThen(Commands.run(() -> {
+            fuel.setIntakeLauncherRoller(FuelConstants.LAUNCHING_LAUNCHER_PERCENT);
+            fuel.setFeederRoller(FuelConstants.INDEXER_LAUNCHING_PERCENT);
+        }, fuel))).onFalse(Commands.runOnce(() -> fuel.stop(), fuel));
+        operator.a().whileTrue(Commands.run(() -> {
+            fuel.setIntakeLauncherRoller(-1 * FuelConstants.INTAKE_EJECT_PERCENT);
+            fuel.setFeederRoller(FuelConstants.INDEXER_LAUNCHING_PERCENT);
+        }, fuel)).onFalse(Commands.runOnce(() -> fuel.stop(), fuel));
+        operator.povUp().whileTrue(Commands.run(() -> {
+            climber.setClimber(ClimbConstatns.CLIMBER_MOTOR_UP_PERCENT);
+        }, fuel)).onFalse(Commands.runOnce(() -> climber.stop(), climber));
+        operator.povDown().whileTrue(Commands.run(() -> {
+            climber.setClimber(ClimbConstatns.CLIMBER_MOTOR_DOWN_PERCENT);
+        }, fuel)).onFalse(Commands.runOnce(() -> climber.stop(), climber));
+    }
 
-    // Switch to X pattern when X button is pressed
-    controller.x()
-        .onTrue(new ActionFactory().singleAction("X-Command", () -> drive.stopWithX(), drive));
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return autoChooser.get();
+    }
 
-    // Reset gyro to 0° when B button is pressed
-    controller.b()
-        .onTrue(new ActionFactory().singleAction("ZeroGyroCommand",
-            () -> drive.setPose(new Pose2d(drive.getPose().getTranslation(), Rotation2d.kZero)),
-            drive).ignoringDisable(true));
+    public Pose2d getPose2D() {
+        return drive.getPose();
+    }
 
-    // Antitipping
-    controller.leftBumper()
-        .whileTrue(new ActionFactory().continuousAction("DriveWithAntiTipping",
-            () -> DriveCommands.joystickDriveWithAntiTipping(drive, () -> -controller.getLeftY(),
-                () -> -controller.getLeftX(), () -> -controller.getRightX(), antiTipping),
-            () -> DriveCommands.joystickDriveWithAntiTipping(drive, () -> 0, () -> 0, () -> 0,
-                antiTipping)));
-  }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    return autoChooser.get();
-  }
-
-  public Pose2d getPose2D() {
-    return drive.getPose();
-  }
-
-  public void teleopPeriodic() {
-    antiTipping.calculate();
-    HubData hubData = hub.getHubData();
-    Logger.recordOutput("Hub/Status", hubData.owner);
-    Logger.recordOutput("Hub/TimeRemaing", hubData.timeRemaining);
-  }
+    public void teleopPeriodic() {
+        antiTipping.calculate();
+        HubData hubData = hub.getHubData();
+        Logger.recordOutput("Hub/Status", hubData.owner);
+        Logger.recordOutput("Hub/TimeRemaing", hubData.timeRemaining);
+    }
 }
