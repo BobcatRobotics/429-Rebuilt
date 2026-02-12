ed5c3b98e14a264e13f04a048cf9c32a082a4867
ImReallyCravingACheesePizzaRightNow <arnav.thakrar@gmail.com>
2026-02-11 20:15:51 -0500

    Motor Tuner Constans Update #1
    Changed the tuner constants for the drivetrain.

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "bff956326dcd8bceaa4ff25a393ebf9b42f2f697";
-  public static final String GIT_DATE = "2026-02-01 12:05:34 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-01 12:45:29 EST";
-  public static final long BUILD_UNIX_TIME = 1769967929890L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "96d840abe1d26da2fffd2147b9f554857ec4f493";
+  public static final String GIT_DATE = "2026-02-01 13:25:05 EST";
+  public static final String GIT_BRANCH = "SwervePhoenixTunerX";
+  public static final String BUILD_DATE = "2026-02-11 20:04:35 EST";
+  public static final long BUILD_UNIX_TIME = 1770858275561L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
