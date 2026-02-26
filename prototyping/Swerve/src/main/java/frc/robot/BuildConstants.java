5edf14800ac7bcddfb750cd7f7b745dbed21befb
WeeMan01008 <emazzie812@gmail.com>
2026-02-25 19:52:16 -0500

    Functional swerve
    Set can ids, set gear ratios, functional test, configuration, fixed offsets

--- prototyping/Swerve/src/main/java/frc/robot/BuildConstants.java
+++ prototyping/Swerve/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Swerve";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "96d840abe1d26da2fffd2147b9f554857ec4f493";
-  public static final String GIT_DATE = "2026-02-01 13:25:05 EST";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "cfe4b7cb5072d612f53f3049529d599625cd3493";
+  public static final String GIT_DATE = "2026-02-12 08:14:13 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-12 08:09:21 EST";
-  public static final long BUILD_UNIX_TIME = 1770901761917L;
+  public static final String BUILD_DATE = "2026-02-25 19:48:53 EST";
+  public static final long BUILD_UNIX_TIME = 1772066933676L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
