7e5016b5bdd672270cea09862fa64819c9068b26
SoxfnLj <michael@michaellillis.com>
2026-03-09 18:40:12 -0400

    teleop reset and orientation working
    teleop orientation working, gyro reset working, autos running backwards and rotating at end.

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "6df6abeadc22078d30449f76a99bf84195d94c72";
-  public static final String GIT_DATE = "2026-03-07 23:24:00 EST";
-  public static final String GIT_BRANCH = "CombinedSubsystems";
-  public static final String BUILD_DATE = "2026-03-08 21:54:09 EDT";
-  public static final long BUILD_UNIX_TIME = 1773021249499L;
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "f1684a6d342e6f57f8bc33b73e0c6242f5621114";
+  public static final String GIT_DATE = "2026-03-09 16:48:06 EDT";
+  public static final String GIT_BRANCH = "Pre-WNEU-Gyro-Fixes";
+  public static final String BUILD_DATE = "2026-03-09 18:33:11 EDT";
+  public static final long BUILD_UNIX_TIME = 1773095591188L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
