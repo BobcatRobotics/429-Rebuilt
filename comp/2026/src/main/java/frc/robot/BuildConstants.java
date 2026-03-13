574ce1762eab3c10a6bdbd0237421cc6fdca9605
arnavdomm-cpu <arnav.domm@gmail.com>
2026-03-12 20:21:02 -0400

    Added Elastic code

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 16;
-  public static final String GIT_SHA = "9004d575d1629ca760dafb7fac8d97b04de99457";
-  public static final String GIT_DATE = "2026-03-10 23:13:23 EDT";
-  public static final String GIT_BRANCH = "Pre-WNEU-Gyro-Fixes";
-  public static final String BUILD_DATE = "2026-03-10 23:56:17 EDT";
-  public static final long BUILD_UNIX_TIME = 1773201377616L;
+  public static final int GIT_REVISION = 21;
+  public static final String GIT_SHA = "60a668cec61287d4ef610467b1f1bb625274e2d6";
+  public static final String GIT_DATE = "2026-03-12 10:29:26 EDT";
+  public static final String GIT_BRANCH = "ArnavElastics";
+  public static final String BUILD_DATE = "2026-03-12 20:16:37 EDT";
+  public static final long BUILD_UNIX_TIME = 1773360997163L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
