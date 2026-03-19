76f76df671adc427748f56d58bdbad8c63941deb
SoxfnLj <michael@michaellillis.com>
2026-03-19 19:59:09 -0400

    Changed climb up and down percents
    Changed the up and down percent so that it climbs in about 5 seconds without breaking the motor

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 26;
-  public static final String GIT_SHA = "339ad0cd0c3f86a547e695b9f35e06b47f410847";
-  public static final String GIT_DATE = "2026-03-19 19:09:06 EDT";
+  public static final int GIT_REVISION = 27;
+  public static final String GIT_SHA = "7387177fc1f688302049bd9fcf22c0c3f83e23d2";
+  public static final String GIT_DATE = "2026-03-19 19:48:22 EDT";
   public static final String GIT_BRANCH = "Climber-Tunning";
-  public static final String BUILD_DATE = "2026-03-19 19:46:42 EDT";
-  public static final long BUILD_UNIX_TIME = 1773964002373L;
+  public static final String BUILD_DATE = "2026-03-19 19:56:52 EDT";
+  public static final long BUILD_UNIX_TIME = 1773964612454L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
