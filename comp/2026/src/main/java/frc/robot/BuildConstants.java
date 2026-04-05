eb7aa37a50e6e6ca3e325be3bebdc88319a8ec53
arastogi11 <82742588+arastogi11@users.noreply.github.com>
2026-04-05 14:38:02 -0400

    sped up akash path and added a waypoint

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 46;
-  public static final String GIT_SHA = "7446c92b5318c7b285769386ba5ca532b07fe1c0";
-  public static final String GIT_DATE = "2026-04-04 23:20:57 EDT";
+  public static final int GIT_REVISION = 47;
+  public static final String GIT_SHA = "09fbd2841fe8fbc0914d4ee9327119b63f7711f1";
+  public static final String GIT_DATE = "2026-04-05 14:17:27 EDT";
   public static final String GIT_BRANCH = "akash_testing_pathplanner";
-  public static final String BUILD_DATE = "2026-04-05 13:48:40 EDT";
-  public static final long BUILD_UNIX_TIME = 1775411320741L;
+  public static final String BUILD_DATE = "2026-04-05 14:22:04 EDT";
+  public static final long BUILD_UNIX_TIME = 1775413324412L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
