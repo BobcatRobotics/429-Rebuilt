1232dd2e25044873fd6e90647b1097bcd083ae19
arastogi11 <82742588+arastogi11@users.noreply.github.com>
2026-04-05 21:44:23 -0400

    sim working with advantagescope but i didn't have a controller athome

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 48;
-  public static final String GIT_SHA = "eb7aa37a50e6e6ca3e325be3bebdc88319a8ec53";
-  public static final String GIT_DATE = "2026-04-05 14:38:02 EDT";
+  public static final int GIT_REVISION = 49;
+  public static final String GIT_SHA = "a527af8e4b9a20b6c786321a298a48c219bae639";
+  public static final String GIT_DATE = "2026-04-05 21:00:43 EDT";
   public static final String GIT_BRANCH = "akash_testing_pathplanner";
-  public static final String BUILD_DATE = "2026-04-05 20:46:01 EDT";
-  public static final long BUILD_UNIX_TIME = 1775436361548L;
+  public static final String BUILD_DATE = "2026-04-05 21:28:41 EDT";
+  public static final long BUILD_UNIX_TIME = 1775438921261L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
