775fe73c31d1f6f253b4568500b614a3fd0f9fd7
AashrithG <gummadiaashrith@gmail.com>
2026-05-13 20:41:02 -0400

    Changed where robot will avoid in pathplanner
    Made robot only be able to go over bump in specific spot and avoid things like hub and trench so the robot will not hit anything while auto climbing

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 85;
-  public static final String GIT_SHA = "a79a9e6ca178e5c270c5cd0236789134c8e34b02";
-  public static final String GIT_DATE = "2026-05-10 11:51:30 EDT";
+  public static final int GIT_REVISION = 86;
+  public static final String GIT_SHA = "7e5f0c0f099d4d42caf49dcc5c94fc3cd37b5d2a";
+  public static final String GIT_DATE = "2026-05-13 20:29:39 EDT";
   public static final String GIT_BRANCH = "Led_Test_Code";
-  public static final String BUILD_DATE = "2026-05-13 19:31:13 EDT";
-  public static final long BUILD_UNIX_TIME = 1778715073626L;
+  public static final String BUILD_DATE = "2026-05-13 20:35:09 EDT";
+  public static final long BUILD_UNIX_TIME = 1778718909930L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
